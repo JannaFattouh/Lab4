8f31ac1f514abb221f5ac4970bd8efe28a42b16c
Janna <87377365+JannaFattouh@users.noreply.github.com>
2023-12-26 00:57:53 +0200

    Add files via upload

---
+++ sssss.sh
@@ -0,0 +1,96 @@
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #d4e9f0;
+           
+
+        }
+
+        header {
+            background-color: #105274;
+            color: #fff;
+            padding: 10px;
+            text-align: left;
+        }
+
+        #logo {
+            display: inline-block;
+            margin-right: 20px;
+        }
+
+        #profile-icon {
+            display: inline-block;
+            margin-right: 20px;
+        }
+
+        a {
+        text-decoration: none; 
+        color: white; 
+        margin: 10px;
+       }
+
+        a:hover {
+        color: #b4d4d8; 
+       }
+
+       h2{
+            color:#385a63;
+        }
+        form {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+
+        #search-form {
+            background-color: #fff;
+            width: 75%;
+            padding: 20px;
+            margin: 60px auto;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center; /* Center the text content within the form */
+        }
+
+        
+        label {
+            text-align: left;
+            font-weight: bold;
+            display: inline-block;
+            box-sizing: border-box;
+            width: auto;
+            margin-bottom: 6px;
+            color: #385a63;
+        }
+        
+
+        input, select {
+            padding: 10px;
+            width: 25%;
+            display: inline-block;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+       
+        #search-btn-container {
+        text-align: center;
+        }
+       
+        #search-btn {
+            background-color: #385a63;
+            color: #fff;
+            padding: 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        #search-btn:hover{
+            background-color:#d4e9f0;
+            color: #385a63;
+        }
+    </style>
